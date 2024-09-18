b29adfd8127425c4bbfbd63f10ed77dce0bdf58e
pjt3591oo <pjt3591oo@gmail.com>
2024-09-18 17:36:18 +0900

    ADDED: collection count

--- collection-peek.py
+++ collection-peek.py
@@ -1,6 +1,11 @@
 import chromadb
 chroma_client = chromadb.HttpClient(host="localhost", port=8000)
-print(chroma_client.list_collections())
+
 
 collection = chroma_client.get_collection("new_collection")
 print(collection.peek())
+
+collection_count = chroma_client.count_collections()
+print(chroma_client.list_collections())
+
+print(collection_count)
